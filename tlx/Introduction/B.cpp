f33fc6613a567280516e7f639e792bb0ef59035a
steven <stevenchristian4565@gmail.com>
2023-09-15 23:01:23 +0700

    1b

--- tlx/Introduction/B.cpp
+++ tlx/Introduction/B.cpp
@@ -12,15 +12,10 @@ bool isFull(vector<bool> array, int length) {
   return rowSum == length;
 }
 
-using namespace std;
-
 int main() {
   int row, col;
-
   cin >> row >> col;
-
-  vector<vector<bool>> matrix(row, vector<bool>(col));
-  int rowCount[row];
+  bool matrix[row][col];
 
   vector<bool> isRowFull(row);
   for (int i = 0; i < row; i++) {
@@ -30,12 +25,47 @@ int main() {
     int rowSum = 0;
     for (int j = 0; j < rowStr.size(); j++) {
       matrix[i][j] = rowStr[j] - '0';
-      rowSum += rowStr[j] - '0';
     }
-    if (rowSum == row) {
-      isRowFull[i] = true;
-    } else
-      isRowFull[i] = false;
+  }
+
+  vector<int> rowSum(row, 0);
+  vector<int> colSum(col, 0);
+
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      int value = matrix[i][j];
+      rowSum[i] += value;
+      colSum[j] += value;
+    }
+  }
+
+  int fullRows = 0;
+  int lowestIdx = -1;
+  for (int i = rowSum.size(); i >= 0; i--) {
+    if (rowSum[i] == col) {
+      fullRows++;
+      lowestIdx = i;
+      for (int j = 0; j < col; j++) {
+        matrix[i][j] = false;
+      }
+    }
+  }
+
+  for (int i = 0; i < col; i++) {
+    for (int j = lowestIdx; j >= 0; j--) {
+      if (matrix[j][i]) {
+        for (int k = j + 1; k <= colSum[i] - fullRows; k++) {
+          matrix[k][i] = true;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      cout << matrix[i][j] + 0;
+    }
+    cout << endl;
   }
 
   return 0;
