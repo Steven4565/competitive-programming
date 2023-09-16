1934a3b27df4fffc6557595c1445f23615e8f9ba
steven <stevenchristian4565@gmail.com>
2023-09-16 13:44:41 +0700

    fixed .vscode

--- tlx/Introduction/D.cpp
+++ tlx/Introduction/D.cpp
@@ -13,12 +13,32 @@ int main() {
 
   int i = 0, j = 0;
   int diffIdx;
+  bool skipped = false;
+  bool valid = true;
+  while (true) {
+    if (a[i] != b[j]) {
+      if (!skipped)
+        skipped = true;
+      else {
+        valid = false;
+      }
+
+      if (a.size() > b.size()) {
+        i++;
+      } else {
+        j++;
+      }
+    } else {
+      j++;
+      i++;
+    }
+  }
   // cout << diffIdxEnd << endl << diffIdxStart << endl;
-  // if (abs(diffIdxEnd - diffIdxStart == 2)) {
-  //   correct();
-  // } else {
-  //   wrong();
-  // }
+  if (valid && skipped) {
+    correct();
+  } else {
+    wrong();
+  }
 
   return 0;
 }
