a56ecee6d783c38026ca9bba701688bedc79151e
Steven4565 <stevenchristian4565@gmail.com>
2023-09-16 13:29:24 +0700

    1d

---
+++ tlx/Introduction/D.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void correct() { cout << "Tentu saja bisa!" << endl; }
+
+void wrong() { cout << "Wah, tidak bisa :(" << endl; }
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+
+  int i = 0, j = 0;
+  int diffIdx;
+  // cout << diffIdxEnd << endl << diffIdxStart << endl;
+  // if (abs(diffIdxEnd - diffIdxStart == 2)) {
+  //   correct();
+  // } else {
+  //   wrong();
+  // }
+
+  return 0;
+}
