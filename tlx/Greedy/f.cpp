345dd4c1b9540583da984b7d5d159b27d6c1a1e8
Steven4565 <stevenchristian4565@gmail.com>
2023-09-30 19:21:17 +0700

    6f

--- tlx/Greedy/f.cpp
+++ tlx/Greedy/f.cpp
@@ -7,14 +7,14 @@ using namespace std;
 typedef unsigned long long ll;
 
 ll n, m;
-pair<int, ll> prices[11];
+pair<ll, ll> prices[11];
 vector<ll> counts(11, 0);
 
-int comp(pair<int, ll> a, pair<int, ll> b) {
+int comp(pair<ll, ll> a, pair<ll, ll> b) {
   return a.second < b.second || (a.second == b.second && a.first > b.first);
 }
 
-int main() {
+signed main() {
   cin >> n;
   n++;
   for (int i = 0; i < n; i++) {
@@ -25,21 +25,17 @@ int main() {
 
   sort(prices, prices + n, comp);
 
-  // for (int i = 0; i < n; i++) {
-  //   cout << prices[i].first << " " << prices[i].second << endl;
-  // }
-
   // Get max number of plates you can buy
   ll leastPrice = prices[0].second;
   counts[prices[0].first] = m / leastPrice;
   ll maxPlates = m / leastPrice;
-
   ll remainder = m % leastPrice;
+
   // cout << "rem " << remainder << endl;
   for (int i = n - 1; i >= 0 && remainder > 0 && maxPlates > 0; i--) {
     // If the previous plate in the sorted array has the same price, then skip.
     // Because it has a lower number.
-    if (i - 1 >= 0 && prices[i - 1].second == prices[i].second) continue;
+    if (i > 0 && prices[i - 1].second == prices[i].second) continue;
 
     ll diff = prices[i].second - leastPrice;
     // cout << "diff " << diff << endl;
@@ -48,21 +44,19 @@ int main() {
     if (counts[prices[0].first] <= 0) break;
 
     // If it can swap, then swap for the higher plate
-    if (diff != 0 && diff <= remainder && remainder != 0) {
+    if (diff != 0 && diff <= remainder && remainder != 0 &&
+        prices[i].first > prices[0].first) {
       ll bought = remainder / diff;
       remainder %= diff;
-
-      cout << "bought " << bought << endl;
+      bought = min(bought, counts[prices[0].first]);
 
       // cout << "bought " << bought << " " << prices[i].first << endl;
       counts[prices[i].first] += bought;
-      counts[prices[0].first] -= bought;  // FIXME: might break if bought more
-                                          // upgrades than the current plates.
+      counts[prices[0].first] -= bought;
     }
   }
 
-  // TODO: Another edge case, if the leastPrice is plate 0, then get the other
-  // lowest plate to maximize the 0
+  // Lowest plate to maximize the 0
   if (prices[0].first == 0 && counts[0] == maxPlates && counts[0] > 0 &&
       n > 1 && m >= prices[1].second) {
     // cout << "passed" << endl;
@@ -73,22 +67,29 @@ int main() {
     }
     // cout << "min " << least << endl;
 
-    ll countReplaced = ceil((double)least / (double)prices[0].second);
+    ll countReplaced = least / prices[0].second;
+    if (least % prices[0].second > 0) countReplaced++;
     ll moneyCanBeSpent = countReplaced * prices[0].second;
-    // cout << "money " << moneyCanBeSpent << endl;
-    pair<int, ll> wantedNumber;
+    // cout << "replaced " << countReplaced << " money " << moneyCanBeSpent
+    //      << endl;
+    pair<ll, ll> wantedNumber;
     for (int i = 0; i < n; i++) {
       // cout << "pr " << prices[i].first << " " << prices[i].second << endl;
       if (prices[i].first == 0) continue;
       if (prices[i].second <= moneyCanBeSpent) {
         wantedNumber = prices[i];
-        // cout << wantedNumber.first << " test" << endl;
         break;
       }
     }
     // cout << "want " << wantedNumber.first << endl;
     counts[0] -= countReplaced;
+    // maxPlates -= countReplaced - 1;
     counts[wantedNumber.first]++;
+  }
+
+  maxPlates = 0;
+  for (int i = 0; i < n; i++) {
+    maxPlates += counts[i];
   }
 
   // Edge case, when nothing is bought
